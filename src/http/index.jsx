78127f52420ca846d4ff8e4a19864afbc65da447
hgjtu <vlrtmsq@gmail.com>
2025-04-28 21:05:22 +0300

    fix

--- src/http/index.jsx
+++ src/http/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:8010/api/v1"
+export const API_URL = import.meta.env.API_URL
 
 const $api = axios.create({
     withCredentials: true,
