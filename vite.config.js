3a3536e6eff09fc6389fee9650d08a31c8e10656
hgjtu <vlrtmsq@gmail.com>
2025-04-28 21:33:44 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.jsx', '.css'],
+    extensions: ['.js', '.jsx', '.css', '.ts', '.tsx'],
   },
 })
